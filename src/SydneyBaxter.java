f6884196a2d0507f8563a2548e6458dd01222d80
SydneyBaxTER <sb0973805@RK03-B1225-04.francistuttle.edu>
2018-01-22 14:40:57 -0600

    first file version

--- src/SydneyBaxter.java
+++ src/SydneyBaxter.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 import java.util.Arrays;
 
 public class SydneyBaxter {
-
+//
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Please enter 10 numbers");
