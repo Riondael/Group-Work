8900df308f03d1c2bc9ab5f202b183add6a4fe30
IsaacDabney <id0967559@RK03-B1225-07.francistuttle.edu>
2018-01-22 14:15:48 -0600

    First file version

---
+++ src/p7IsaacDabney.java
@@ -0,0 +1,36 @@
+//Owner: Isaac Dabney
+import java.util.Scanner;
+public class p7IsaacDabney 
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter amount of #'s to be read>> ");
+        int[] array = new int[input.nextInt()];
+        readArray(array, input);
+        System.out.printf("The average is: %s", + calculateAverage(array, input));
+        showArray(array);
+    }
+    public static void readArray(int[] array, Scanner input)
+    {
+        for(int x = 0; x < array.length; x++)
+        {
+            System.out.print("Enter Number>> "); 
+            array[x] = input.nextInt();
+        }
+    }
+    public static double calculateAverage(int[] array,Scanner input)
+    {
+        int sum = 0;
+        for(int i = 0; i < array.length;i++)
+        {
+            sum += array[i];
+        }
+        double average = sum/array.length;
+        return average;
+    }
+    public static void showArray(int[] array)
+    {
+        System.out.println(Arrays.toString(array));
+    }
+}
